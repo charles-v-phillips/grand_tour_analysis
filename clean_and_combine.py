817c11e9a60a328c7824a48f8fab964ac4e88cf0
charles phillips <charlesvphillips@gmail.com>
2021-10-15 09:43:03 -0400

    -fixed minor issue with non unique indices in pandas dataframe

--- clean_and_combine.py
+++ clean_and_combine.py
@@ -1,44 +1,80 @@
 import pandas as pd
 import numpy as np
-"""Open All DataFiles and put them into all_data DataFrame"""
-tour_data = pd.read_csv('data/tour-data.csv')
-vuelta_data = pd.read_csv('data/vuelta-data.csv')
-giro_data = pd.read_csv('data/giro-data.csv')
-all_data = pd.concat([tour_data, vuelta_data, giro_data])
+
+
+def combine_and_clean(f1,f2,f3,save_to_file):
+    tour_data = pd.read_csv(f1)
+    vuelta_data = pd.read_csv(f2)
+    giro_data = pd.read_csv(f3)
+    all_data = pd.concat([tour_data, vuelta_data, giro_data])
+
+
+    # Strip 'k' from distance and cast to float
+    extr = tour_data['distance'].str.extract('^  ([\d]+.[\d]*)', expand=False)
+    tour_data['distance'] = pd.to_numeric(extr)
+
+    # Strip 'kph' from distance and cast to float
+    extr = all_data['avg_speed_of_winner'].str.extract('^ ([\d]+.[\d]*)', expand=False)
+    all_data['avg_speed_of_winner'] = pd.to_numeric(extr)
+
+    extr = all_data['vertical_meters']
+    extr = extr.apply(lambda s: s.strip())
+    extr = extr.replace('', np.nan)
+    extr = extr.apply(lambda s: float(s))
+    all_data['vertical_meters'] = extr
+
+    indeces1 = all_data[all_data['winner_age_when_won'] < 10].index
+    indeces2 = all_data[all_data['second_age_when_won'] < 10].index
+    indeces3 = all_data[all_data['third_age_when_won'] < 10].index
+    all_data.drop(indeces1, inplace=True)
+    all_data.drop(indeces2, inplace=True)
+    all_data.drop(indeces3, inplace=True)
+
+
+    all_data.to_csv(save_to_file, index=False)
 
 
 
-"""Cleaning Section"""
+# """Open All DataFiles and put them into all_data DataFrame"""
+# tour_data = pd.read_csv('data/tour-data.csv')
+# vuelta_data = pd.read_csv('data/vuelta-data.csv')
+# giro_data = pd.read_csv('data/giro-data.csv')
+# all_data = pd.concat([tour_data, vuelta_data, giro_data])
+#
+#
+#
+# """Cleaning Section"""
+#
+# #Strip 'kph' from avg_speed and cast to float
+# extr = tour_data['avg_speed_of_winner'].str.extract('^ ([\d]+.[\d]*)', expand=False)
+# tour_data['avg_speed_of_winner'] = pd.to_numeric(extr)
+# tour_data['avg_speed_of_winner'] = pd.to_numeric(extr)
+#
+# #Strip 'k' from distance and cast to float
+# extr = tour_data['distance'].str.extract('^  ([\d]+.[\d]*)', expand=False)
+# tour_data['distance'] = pd.to_numeric(extr)
+#
+# #Strip 'kph' from distance and cast to float
+# extr = all_data['avg_speed_of_winner'].str.extract('^ ([\d]+.[\d]*)', expand=False)
+# all_data['avg_speed_of_winner'] = pd.to_numeric(extr)
+# all_data['avg_speed_of_winner'] = pd.to_numeric(extr)
+#
+#
+# #Strip
+# extr = all_data['distance'].str.extract('^  ([\d]+.[\d]*)', expand=False)
+# all_data['distance'] = pd.to_numeric(extr)
+#
+#
+#
+# extr = all_data['vertical_meters']
+# extr = extr.apply(lambda s : s.strip())
+# extr = extr.replace('',np.nan)
+# extr = extr.apply(lambda s : float(s))
+# all_data['vertical_meters'] = extr
+#
+# all_data.to_csv('data/all_data.csv')
 
-#Strip 'kph' from avg_speed and cast to float
-extr = tour_data['avg_speed_of_winner'].str.extract('^ ([\d]+.[\d]*)', expand=False)
-tour_data['avg_speed_of_winner'] = pd.to_numeric(extr)
-tour_data['avg_speed_of_winner'] = pd.to_numeric(extr)
-
-#Strip 'k' from distance and cast to float
-extr = tour_data['distance'].str.extract('^  ([\d]+.[\d]*)', expand=False)
-tour_data['distance'] = pd.to_numeric(extr)
-
-#Strip 'k' from distance and cast to float
-extr = all_data['avg_speed_of_winner'].str.extract('^ ([\d]+.[\d]*)', expand=False)
-all_data['avg_speed_of_winner'] = pd.to_numeric(extr)
-all_data['avg_speed_of_winner'] = pd.to_numeric(extr)
-
-
-#Strip
-extr = all_data['distance'].str.extract('^  ([\d]+.[\d]*)', expand=False)
-all_data['distance'] = pd.to_numeric(extr)
-
-
-
-extr = all_data['vertical_meters']
-extr = extr.apply(lambda s : s.strip())
-extr = extr.replace('',np.nan)
-extr = extr.apply(lambda s : float(s))
-all_data['vertical_meters'] = extr
-
-all_data.to_csv('data/all_data.csv')
-
+combine_and_clean('testing_giro.csv','testing_tour.csv','testing_vuelta.csv', 'data/try_again2.csv')
 
 if __name__ == '__main__':
     pass
--- visuals/age_spread_hist_plot.py
+++ visuals/age_spread_hist_plot.py
@@ -3,9 +3,10 @@ import numpy as np
 from matplotlib import pyplot as plt
 import seaborn as sns
 
-all_data = pd.read_csv('/Users/charlesphillips/nycdsa/project_1_pcs/data/all_data.csv')
-
-hist = sns.histplot(data = all_data, x = 'winner_age_when_won', kde = True)
+all_data = pd.read_csv('/Users/charlesphillips/nycdsa/project_1_pcs/data/try_again2.csv')
+ages = pd.concat([all_data['winner_age_when_won'],all_data['second_age_when_won'],all_data['third_age_when_won']])
+# .reset_index(drop=True)
+hist = sns.histplot(ages, kde = True, bins = 20)
 sns.despine()
 
 # ax = plt.axes()
@@ -23,9 +24,9 @@ hist.set_ylabel('Count')
 
 
 
+plt.show()
 
-
-plt.savefig('hist_of_age_of_winner.png',transparent = True)
+# plt.savefig('hist_of_age_of_winner.png',transparent = True)
 
 if __name__ == '__main__':
     pass
